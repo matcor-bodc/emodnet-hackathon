692a0359852b3fffcdf879ab0d64973266480a5a
Matt McCormack <matcor@noc.ac.uk>
2023-03-28 12:48:00 +0100

    Add emodnet data to the map

--- src/components/ReportMap.tsx
+++ src/components/ReportMap.tsx
@@ -9,11 +9,12 @@ import VectorLayer from 'ol/layer/Vector'
 import { fromLonLat } from 'ol/proj'
 import OSM from 'ol/source/OSM'
 import VectorSource from 'ol/source/Vector'
+import TileWMS from 'ol/source/TileWMS.js';
 
 const COLOURS: any = {
   Animal: 'red',
   Litter: 'magenta',
-  Other: 'cyan'
+  Other: 'purple'
 }
 
 interface Props {
@@ -32,6 +33,16 @@ export const ReportMap = ({ data, setSelectedId }: Props) => {
     })
     baseLayer.set('name', 'base-layer')
 
+    const layer = new TileLayer({
+      source: new TileWMS({
+        url: 'https://ows.emodnet-humanactivities.eu/wms',
+        params: {'LAYERS': 'vesseldensity_allavg', 'TILED': true},
+        serverType: 'geoserver',
+        transition: 0
+      }),
+    })
+    layer.set('name', 'base-layer')
+
     const initialMap = new OlMap({
       target: 'map',
       layers: [
@@ -42,6 +53,8 @@ export const ReportMap = ({ data, setSelectedId }: Props) => {
         zoom: 2
       })
     })
+
+    initialMap.addLayer(layer)
 
     setMap(initialMap)
   }, [])
@@ -65,8 +78,10 @@ export const ReportMap = ({ data, setSelectedId }: Props) => {
       markerLayers[x] = new VectorLayer({
         source: new VectorSource(),
         style: {
-          'circle-radius': 9,
-          'circle-fill-color': COLOURS[x]
+          'circle-radius': 11,
+          'circle-fill-color': COLOURS[x],
+          'circle-stroke-color': '#eee',
+          'circle-stroke-width': 3
         }
       })
     })
